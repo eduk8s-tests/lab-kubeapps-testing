f6fd602cb264ab006aac4baff81b20997cf8ec10
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-10-08 08:49:11 +1100

    Generate config.js file from setup script.

---
+++ workshop/setup.d/generate-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+KUBEAPPS_TOKEN=`kubectl get secret $(kubectl get serviceaccount kubeapps-user -o
+jsonpath='{.secrets[].name}' -n ${SESSION_NAMESPACE}) -o jsonpath='{.data.token}'
+-n ${SESSION_NAMESPACE} | base64 --decode`
+
+export KUBEAPPS_TOKEN
+
+envsubst < $WORKSHOP_DIR/config.js.in > $WORKSHOP_DIR/config.js
